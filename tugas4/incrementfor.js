04e15a6e59a9eebf2d9625bdff54b715b0a22a11
prasetyarahmad <rahmadpras22@gmail.com>
2022-05-31 10:32:35 +0700

    update all

---
+++ tugas4/incrementfor.js
@@ -0,0 +1,9 @@
+for(nilaiAwal=1; nilaiAwal<=10;nilaiAwal++){
+    if(nilaiAwal<=6){
+        console.log('Angkot No. '+nilaiAwal+' beroperasi dengan baik.');
+    }else if(nilaiAwal==8){
+        console.log('Angkot No. '+nilaiAwal+' sedang lembur biar dapur ngebul.');
+    }else{
+        console.log('Angkot No. '+nilaiAwal+' sedang tidak beroperasi.');
+    }
+}
